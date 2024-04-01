51557fafcf709260866a2065da4e732a8b89e0b0
KihaJeong1994 <jkha7371@gmail.com>
2024-04-01 18:47:50 +0900

    get user input from keyboard and print it

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,3 +1,22 @@
+use std::io; // bring io library in standard library into scope
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // let : a statement to create variable
+    // variables are immutable by default
+    // mut : mutable
+    // String::new : new is an associated function of the String type
+    // associated function : a function that is implemented on a type
+    let mut guess = String::new();
+
+    // & : indicates that an argument is a reference.
+    // reference is immutable by default
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
